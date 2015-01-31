e79bc4ab657fb0f43f718d634ecc5b63272c1098
iobh <iob.hartt@gmail.com>
2015-01-31 19:31:31 +0000

    inscription : CA MAAARCH A 3chirééé!!

---
+++ src/main/java/com/archive/spring/ChangeAccountForm.java
@@ -0,0 +1,66 @@
+package com.archive.spring;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+ 
+@ManagedBean
+public class ChangeAccountForm {
+    
+    private String name;
+    
+    private String email;
+    
+    private String decription;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+    
+    
+    
+    private UploadedFile file;
+ 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+    
+    public void upload() {
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+ 
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
